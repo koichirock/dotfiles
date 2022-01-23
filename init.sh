b644671562e9af754c38a38ed5f1c71ec2fa3fc9
Koichiro Okamoto <koichirookamoto@icloud.com>
2022-01-24 00:00:49 +0900

    Install some zsh plugins

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ pushd $(dirname $0)
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 mv .zshrc ~/.zshrc
 md $ZSH/completions
 source ~/.zshrc
